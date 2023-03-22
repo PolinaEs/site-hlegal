e40b881faec389adee1ef3f2213d858623cc864f
PolinaEs <polina5785@gmail.com>
2023-03-22 21:26:07 +0300

    home-commit-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,13 @@ const body = document.querySelector('body')
 
 menuBtn.addEventListener('click', (e) => {
   menuBtn.classList.toggle('active')
-  body.classList.toggle('active')
-  if (menuList.classList.contains('active')) {
-    setTimeout(() => menuList.classList.toggle('active'), 500)
-  } else {
-    menuList.classList.toggle('active')
-  }
+
 })
+
+$('.menu__btn').on('click', function () {
+  $(this).toggleClass('-active');
+  $('.menu__list').stop(true, true).slideToggle(500);
+});
 
 var btnContainer = document.getElementById("active-lang");
 var btns = btnContainer.getElementsByClassName("actions__language-link");
@@ -21,6 +21,35 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+// get DOM elements
+const headersList = document.querySelectorAll('.slider__header')
+const notesList = document.querySelectorAll('.slider__note')
+const indicatorsList = document.querySelectorAll('.slider__indicator')
+const descriptionsList = document.querySelectorAll('.slider__description')
+
+let index = 0;
+let interval = 5500;
+let heightsArr = [];
+let heightMax = null;
+
+setInterval(() => {
+  // remove the active classes for the first elements
+  headersList[index].classList.toggle('active')
+  notesList[index].classList.toggle('active')
+  indicatorsList[index].classList.toggle('active')
+  // increase the index until the number of elements is exceeded
+  index = (index + 1) % headersList.length;
+  // set active classes to the following elements
+  headersList[index].classList.toggle('active')
+  notesList[index].classList.toggle('active')
+  indicatorsList[index].classList.toggle('active')
+}, interval)
+
+// calculating and changing the height of the description block for the maximum text
+descriptionsList.forEach(el => heightsArr.push(el.clientHeight))
+heightMax = Math.max(...heightsArr)
+descriptionsList.forEach(el => el.style.height = `${heightMax}px`)
 
 
 
